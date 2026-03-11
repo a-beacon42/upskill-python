0d26a448e56418ff8cb7d8f9b5b27ca4c1b653d1
Alex Beacon <a.beacon42@gmail.com>
2026-03-11 08:36:13 -0400

    pretest

--- src/app/models.py
+++ src/app/models.py
@@ -15,6 +15,14 @@ class Product:
     tags: Optional[list[str]] = field(default_factory=list)
     sku: uuid.UUID = field(default_factory=uuid.uuid4)
 
+    def to_dict(self):
+        return {
+            "name": self.name,
+            "price": self.price,
+            "quantity": self.quantity,
+            "tags": self.tags,
+        }
+
     def __post_init__(self):
         if self.quantity < 1:
             raise InvalidProductError(
@@ -28,3 +36,20 @@ class Product:
             raise InvalidProductError(
                 f"[{self.sku}] Invalid product: price must be greater than 0.00"
             )
+
+
+import pytest
+
+
+def test_valid_product():
+    product_fields = {
+        "name": "name",
+        "price": 4.24,
+        "quantity": 2,
+        "tags": ["sale", "popular"],
+    }
+    new_prod = Product(**product_fields)
+    assert new_prod.to_dict() == product_fields
+
+
+def test_invalid_quantity_raises(): ...
